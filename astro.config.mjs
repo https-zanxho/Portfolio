288f03ce9150f61e87c915a8133a58e54209263a
https-zanxho <valentano0710@gmail.com>
2025-10-29 02:54:09 +0100

    update site:URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   // Define un site válido para evitar "Invalid URL" cuando algún código use import.meta.env.SITE
-  site: 'http://localhost:4321/en',
+  site: 'https://portfolio-valentino.vercel.app',
   integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()], // con estas versiones, esto funciona sin cast
